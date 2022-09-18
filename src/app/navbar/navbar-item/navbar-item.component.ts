cdb4aada4de895eb825e3dbc6fac163e69d57a96
Walter Cassarà <c.walter98@hotmail.it>
2022-09-18 17:49:00 +0200

    Navbar: iniziata

---
+++ src/app/navbar/navbar-item/navbar-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-item',
+  templateUrl: './navbar-item.component.html',
+  styleUrls: ['./navbar-item.component.scss']
+})
+export class NavbarItemComponent implements OnInit
+{
+  @Input() href: string | undefined;
+  @Input() icon: string | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
