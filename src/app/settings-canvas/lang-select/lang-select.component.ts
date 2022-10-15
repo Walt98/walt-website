a1b9e2b8c1f7b56946b22858c5b979ad441753a2
Walter Cassarà <c.walter98@hotmail.it>
2022-10-15 17:19:32 +0200

    Sistemato stile lang-select

--- src/app/settings-canvas/lang-select/lang-select.component.ts
+++ src/app/settings-canvas/lang-select/lang-select.component.ts
@@ -12,7 +12,6 @@ export class LangSelectComponent implements OnInit
   public darkMode?: boolean;
   public palette: any;
 
-  // public radialColor?: string;
   public countryCode: string = 'it';
   public language: string = 'ITA';
 
@@ -25,32 +24,9 @@ export class LangSelectComponent implements OnInit
 
   ngOnInit(): void
   {
-    // this.appService.lang$.subscribe((lang: string) => this.countryCode = lang == 'it' ? 'it' : 'gb');
     this.countryCode = this.translate.currentLang == 'it' ? 'it' : 'gb';
     this.language = this.translate.currentLang == 'it' ? 'ITA' : 'ENG';
     this.appService.darkMode$.subscribe((darkMode: string) => this.darkMode = darkMode == 'on');
-    // this.appService.palette$.subscribe((palette: any) =>
-    // {
-    //   this.palette = palette;
-    //   switch (palette.color)
-    //   {
-    //     case '#b44b4b':
-    //       this.radialColor = 'radial-red';
-    //       break;
-    //     case '#67b34d':
-    //       this.radialColor = 'radial-green';
-    //       break;
-    //     case '#b39c4a':
-    //       this.radialColor = 'radial-yellow';
-    //       break;
-    //     case '#aa85bd':
-    //       this.radialColor = 'radial-purple';
-    //       break;
-    //     default:
-    //       this.radialColor = 'radial-default';
-    //       break;
-    //   }
-    // });
   }
 
   setLanguage(country: string)
