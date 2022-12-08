f9c86d23994a8a849c23b2c00a085a5ca99c4a31
Walter Cassarà <c.walter98@hotmail.it>
2022-12-08 23:20:57 +0100

    Piccoli cambiamenti

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,7 +10,10 @@ export class HomeComponent implements OnInit
 {
   public palette: any;
   public gradient?: string;
-  public darkMode?: boolean;
+  public darkMode: boolean = false;
+  public font: string = 'Montserrat';
+
+  public titleStyle?: string;
 
   constructor(private appService: AppService) { }
 
@@ -20,14 +23,32 @@ export class HomeComponent implements OnInit
     {
       this.palette = palette;
       this.gradient = palette.bgImage;
+      this.setTitleStyle();
     });
+
     this.appService.darkMode$.subscribe(value =>
     {
       this.darkMode = value == "on";
-      this.gradient = this.darkMode
-      ? 'linear-gradient(147.38deg, rgb(157 176 185) 0%, rgb(54 93 120) 100%)'
-      : this.palette.bgImage;
+      this.setTitleStyle();
+    });
+
+    this.appService.font$.subscribe(font =>
+    {
+      this.font = font;
+      this.setTitleStyle();
     });
   }
 
+  public setTitleStyle()
+  {
+    let arr: string[] = [];
+    arr.push(
+      `background-image: ${this.darkMode
+        ? 'linear-gradient(147.38deg, rgb(157 176 185) 0%, rgb(54 93 120) 100%)'
+        : this.palette.bgImage}`,
+      `font-weight: ${this.font == "Roboto" ? 800 : 700}`,
+    );
+
+    this.titleStyle = arr.join("; ");
+  }
 }
