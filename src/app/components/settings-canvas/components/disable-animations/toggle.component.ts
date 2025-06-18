8f5a816e22a19c55f2e1da1e1d54b98024c9acba
Walter Cassarà <walter@Mac.home>
2025-06-18 16:51:37 +0200

    commit

---
+++ src/app/components/settings-canvas/components/disable-animations/toggle.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { BaseDirective } from 'src/app/directives/base.directive';
+
+@Component({
+  selector: 'app-toggle',
+  templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.scss']
+})
+export class ToggleComponent extends BaseDirective implements OnInit
+{
+  ngOnInit()
+  {
+    this.$.Palette();
+    this.$.DarkMode();
+  }
+
+  public setDarkMode()
+  {
+    this.Customizer.DarkMode = !this.Customizer.DarkMode;
+    this._payload.set$.darkMode(this.Customizer.DarkMode ? "on" : "off");
+
+    if (this.Customizer.DarkMode)
+    {
+      document.documentElement.style.setProperty(`--gradient`, "linear-gradient(147.38deg, #a4c8d8 0%, #4989b9 100%)");
+    }
+  }
+}
